838cefcedc2b3eaa1a9e28ad578f3670d5ae3339
Brian Coe <bwc126@gmail.com>
2016-10-06 00:52:12 -0400

    docs: Inline comments for loader.js

--- js/loader.js
+++ js/loader.js
@@ -4,12 +4,14 @@
   var images = [];
   var numImg = 0;
   var imgList = [];
+  // @function preload will create an Image object for each source passed to it, preloading it off-screen while the page finishes loading and the user browses the initial content, helping to create a seamless user experience.
   function preload(imgArray) {
     for (var i = 0; i < imgArray.length; i++) {
       images[i] = new Image();
       images[i].src = imgArray[i];
     }
   };
+  // @function getImages takes in the collection of projects, and extracts the relevant image sources from each one.
   function getImages(coll) {
   numImg = coll.length;
     for (var i = 0; i < numImg; i++) {
